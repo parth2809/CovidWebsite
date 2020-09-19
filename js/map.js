69b742ba76502ffd053620dbcf976673f94b9306
Najeem Kanishka <contact@najeemkanishka.com>
2020-09-19 07:56:57 +0000

    Added more properly working buttons for toggle

--- js/map.js
+++ js/map.js
@@ -1,10 +1,17 @@
-var week1 = [];
-var week2 = [];
-var week3 = [];
-var week4 = [];
+var activeType = 1 //1 is Incidental, 0 is Cumulative
 
-function chartMap(data) {
-    // Instantiate the map
+var week1_inc = [];
+var week2_inc = [];
+var week3_inc = [];
+var week4_inc = [];
+
+var week1_cum = [];
+var week2_cum = [];
+var week3_cum = [];
+var week4_cum = [];
+
+function chartMap(data, week) {
+    // Draws the map, using weekly data and the given week
     Highcharts.mapChart('map', {
 
         chart: {
@@ -13,7 +20,9 @@ function chartMap(data) {
         },
 
         title: {
-            text: 'Point Estimates for Incidental Deaths 1 Week Ahead 9/7/20'
+            text: 'Point Estimates for ' + (activeType > 0 ? 'Incidental' : 'Cumulative') + ' Deaths ' + 
+            week + (week > 1 ? ' Weeks' : ' Week')
+            + ' Ahead 9/7/20'
         },
 
         exporting: {
@@ -27,7 +36,7 @@ function chartMap(data) {
             backgroundColor: 'rgba(255,255,255,0.85)',
             floating: true,
             verticalAlign: 'top',
-            y: 25
+            y: 20
         },
 
         mapNavigation: {
@@ -36,6 +45,7 @@ function chartMap(data) {
 
         colorAxis: {
             min: 1,
+            type: 'linear',
             minColor: '#EEEEFF',
             maxColor: '#000022',
             stops: [
@@ -51,14 +61,14 @@ function chartMap(data) {
             dataLabels: {
                 enabled: true,
                 color: '#FFFFFF',
-                format: ''
+                format: '{point.name}',
             },
             borderColor: 'black',
             borderWidth: 0.2,
             name: 'Point Estimate',
             tooltip: {
-                pointFormat: '{point.code}: {point.value}',
-                valueDecimals: 2,
+                pointFormat: '{point.name}: {point.value}',
+                valueDecimals: 3,
             }
         }]
     });
@@ -70,29 +80,70 @@ async function loadJSON(path) {
 	return dataset;
 }
 
-function selectType(chart) {
-    let dfPromise;
-    if (chart == 0) {
-        dfPromise = loadJSON('http://192.168.0.130:8080/datasets/df_inc.json')
+function directState(week) {
+    // sets and updates the chart with the appropriate type and week number, and then draws the map
+    // activeType determines incidental or cumulative
+    // week determines week number
+    if (activeType == 0) {
+        switch (week) {
+            case 1:
+                chartMap(week1_cum, week);
+                break;
+            case 2:
+                chartMap(week2_cum, week);
+                break;
+            case 3:
+                chartMap(week3_cum, week);
+                break;
+            case 4:
+                chartMap(week4_cum, week);
+                break;
+        }
+    } else {
+        switch (week) {
+            case 1:
+                chartMap(week1_inc, week);
+                break;
+            case 2:
+                chartMap(week2_inc, week);
+                break;
+            case 3:
+                chartMap(week3_inc, week);
+                break;
+            case 4:
+                chartMap(week4_inc, week);
+                break;
+        }
     }
-    else {
-        dfPromise = loadJSON('http://192.168.0.130:8080/datasets/df_cum.json')
-    }
-
-    week1 = [];
-    week2 = [];
-    week3 = [];
-    week4 = [];
-
-    dfPromise.then(function (df) {
-        for (const state of df) {
-            week1.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week1"]});
-            week2.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week2"]});
-            week3.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week3"]});
-            week4.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week4"]});
-        };
-        chartMap(week3)
-    });
 }
 
-selectType(1)
+function setActiveType(level) {
+    // Sets the activeType (1 is Incidental, 0 is Cumulative)
+    activeType = level;
+}
+
+
+dfPromiseInc = loadJSON('http://192.168.0.130:8080/datasets/df_inc.json')
+dfPromiseCum = loadJSON('http://192.168.0.130:8080/datasets/df_cum.json')
+
+// Will wait for and then append to respective array
+dfPromiseInc.then(function (df) {
+    // Incidentals
+    for (const state of df) {
+        week1_inc.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week1"]});
+        week2_inc.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week2"]});
+        week3_inc.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week3"]});
+        week4_inc.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week4"]});
+    };
+    directState(1); // Draws the map
+});
+
+dfPromiseCum.then(function (df) {
+    // Cumulative
+    for (const state of df) {
+        week1_cum.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week1"]});
+        week2_cum.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week2"]});
+        week3_cum.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week3"]});
+        week4_cum.push({"code": "us-" + state["code"].toLowerCase(),"value": state["week4"]});
+    };
+});

f67bf9a2ac3451f62a5afe251f8d8f043a93ffd5
Najeem <contact@najeemkanishka.com>
2021-03-19 21:22:35 -0700

    updated data

--- js/dates.js
+++ js/dates.js
@@ -1,1 +1,1 @@
-var dateOfForecast = "03/08/21";var dateOfUpdate = "03/11/21";document.getElementById("update-date").innerText = "Updated: " + dateOfUpdate
+var dateOfForecast = "03/15/21";var dateOfUpdate = "03/20/21";document.getElementById("update-date").innerText = "Updated: " + dateOfUpdate

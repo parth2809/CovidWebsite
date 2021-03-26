038282359764f00e6b7ed11c583b3ec4810755f6
Najeem <contact@najeemkanishka.com>
2021-03-25 22:50:29 -0700

    updated dataset

--- js/dates.js
+++ js/dates.js
@@ -1,1 +1,1 @@
-var dateOfForecast = "03/15/21";var dateOfUpdate = "03/20/21";document.getElementById("update-date").innerText = "Updated: " + dateOfUpdate
+var dateOfForecast = "03/22/21";var dateOfUpdate = "03/26/21";document.getElementById("update-date").innerText = "Updated: " + dateOfUpdate
